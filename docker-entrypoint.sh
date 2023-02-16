33c11d5e6772425961d0e7f38a816ca6be5a91f4
FarisZR <35614734+FarisZR@users.noreply.github.com>
2023-02-16 16:49:33 +0300

    Skip Deployment_command variable to support swarm

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -94,10 +94,10 @@ if  [ -n "$INPUT_DOCKER_LOGIN_PASSWORD" ] || [ -n "$INPUT_DOCKER_LOGIN_USER" ] |
   docker login -u "$INPUT_DOCKER_LOGIN_USER" -p "$INPUT_DOCKER_LOGIN_PASSWORD" "$INPUT_DOCKER_LOGIN_REGISTRY"
 fi
 
-echo "Command: ${DEPLOYMENT_COMMAND} pull"
-${DEPLOYMENT_COMMAND} pull
+echo "Command: docker compose -f $COMPOSE_FILE pull"
+docker compose -f $COMPOSE_FILE pull
 
-echo "Command: ${DEPLOYMENT_COMMAND} ${INPUT_ARGS}"
-${DEPLOYMENT_COMMAND} ${INPUT_ARGS}
+echo "Command: docker compose -f $COMPOSE_FILE ${INPUT_ARGS}"
+docker compose -f $COMPOSE_FILE ${INPUT_ARGS}
 
 
